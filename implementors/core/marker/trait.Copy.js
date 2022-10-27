fd28eb1574c6902d7b9bc4fdccda6437918792cb
GitHub Action <action@github.com>
2022-10-27 13:47:22 +0000

    deploy

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["bpaf"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"bpaf/enum.CompleteDecor.html\" title=\"enum bpaf::CompleteDecor\">CompleteDecor</a>","synthetic":false,"types":["bpaf::CompleteDecor"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"bpaf":["",[],["_applicative.rs","_combinatoric_tutorial.rs","_derive_tutorial.rs","_flow.rs","_unusual.rs","arg.rs","args.rs","batteries.rs","color.rs","complete_gen.rs","complete_run.rs","from_os_str.rs","info.rs","item.rs","lib.rs","meta.rs","meta_help.rs","meta_usage.rs","meta_youmean.rs","params.rs","structs.rs"]]\
+}');
+createSourceSidebar();
